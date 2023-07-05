5253f5944a83d75969467052e7bf19eafeae7ab2
yougnho <yhl1173@naver.com>
2023-07-05 14:13:15 +0900

    123

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://yhl1173:fkaus55@cluster0.tm4fthb.mongodb.net/').then(() => console.log("MongoDB Connected.."))
@@ -8,6 +14,25 @@ mongoose.connect('mongodb+srv://yhl1173:fkaus55@cluster0.tm4fthb.mongodb.net/').
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', async (req, res) => {
+  //회원가입시 필요 정보를 client에서 가져오면
+  //데이터베이스에 삽입한다
+
+
+  //body parser를 통해 body에 담긴 정보를 가져온다
+  const user = new User(req.body)
+
+  //mongoDB 메서드, user모델에 저장
+  console.log("확인 : " + res.status)
+  const result = await user.save().then(()=>{
+    res.status(200).json({
+      success: true
+    })
+  }).catch((err)=>{
+    res.json({ success: false, err })
+  })
 })
 
 app.listen(port, () => {
